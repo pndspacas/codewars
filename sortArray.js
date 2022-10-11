da4c4163acbd0ba2febef1c59bc82e3cbb02fb32
Pedro Paças <99020754+pndspacas@users.noreply.github.com>
2022-10-11 14:49:56 +0100

    Update sortArray.js

--- sortArray.js
+++ sortArray.js
@@ -1,4 +1,4 @@
-/* */
+/* Just a simple sorting usage. Create a function that returns the elements of the input-array / list sorted in lexicographical order. */
 
 // input: names - unsorted array
 // output: sorted array
