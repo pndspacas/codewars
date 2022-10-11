4e6218bce1422a6291b349a9c2655ca9188db977
Pedro Paças <99020754+pndspacas@users.noreply.github.com>
2022-10-11 15:16:49 +0100

    Create checkFactor.js

---
+++ checkFactor.js
@@ -0,0 +1,15 @@
+/* This function should test if the factor is a factor of base.
+
+Return true if it is a factor or false if it is not. */
+
+
+//Answer
+
+function checkForFactor (base, factor) {
+  if (base % factor === 0){
+      return true
+    }
+  else {
+    return false
+  }
+}
