2a0e56a29506fec9ef82df61592e8b70abb09b2d
Pedro Paças <99020754+pndspacas@users.noreply.github.com>
2022-10-11 14:47:51 +0100

    Create csvArray.js

---
+++ csvArray.js
@@ -0,0 +1,8 @@
+/* Create a function that returns the CSV representation of a two-dimensional numeric array. */
+
+//Answer
+
+function toCsvText(array) {
+   let arr = array.join("\n")
+   return arr
+}
