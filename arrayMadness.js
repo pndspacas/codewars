6d9585cf7c69026ca8c20e035fe7e7fe678b87d1
Pedro Paças <99020754+pndspacas@users.noreply.github.com>
2022-10-11 14:39:12 +0100

    Update arrayMadness.js

--- arrayMadness.js
+++ arrayMadness.js
@@ -1,5 +1,8 @@
 /* Given two integer arrays a, b, both of length >= 1, create a program that returns true if the sum of the squares of each element in a is strictly greater than the sum of the cubes of each element in b. */
 
+
+//Answer
+
 function arrayMadness(a, b) {
   let sumA = a.reduce ((acc,c) => acc + Math.pow(c,2),0)
   let sumB = b.reduce ((acc,c) => acc + Math.pow(c,3),0)
