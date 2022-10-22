3ee812b89cc5fc45ea6c5b6829b52e57aa13e462
Pedro Paças <99020754+pndspacas@users.noreply.github.com>
2022-10-22 03:20:21 +0100

    Update addItemToArray.js

--- addItemToArray.js
+++ addItemToArray.js
@@ -1,5 +1,5 @@
 /* Add the value "codewars" to the websites array.
-After your code executes the websites array should == ["codewars"]
+After your code executes the websites array should == ["codewars"] 
 
 The websites array has already been defined for you using the following code:
 */ 
