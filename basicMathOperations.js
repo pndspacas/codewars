e147393e25aec6d896f6c61a2a38d2b68b3efbfa
Pedro Paças <99020754+pndspacas@users.noreply.github.com>
2022-10-22 03:21:20 +0100

    Update basicMathOperations.js

--- basicMathOperations.js
+++ basicMathOperations.js
@@ -3,7 +3,7 @@
 The function should take three arguments - operation(string/char), value1(number), value2(number).
 The function should return result of numbers after applying the chosen operation. */
 
-
+ 
 //Answer
 
 function basicOp(operation, value1, value2)
