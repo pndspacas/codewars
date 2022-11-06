8802a149d676c0f5c1163b2db5c80e41f874e351
Pedro Paças <99020754+pndspacas@users.noreply.github.com>
2022-11-06 04:35:35 +0000

    Update addItemToArray.js

--- addItemToArray.js
+++ addItemToArray.js
@@ -3,7 +3,7 @@ After your code executes the websites array should == ["codewars"]
 
 The websites array has already been defined for you using the following code:
 */ 
-
+   
 //Answer  
 
 let websites = [] 
